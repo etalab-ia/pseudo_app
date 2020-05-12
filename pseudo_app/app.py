938ab25c78812174bb9e96c0f62845798ee6f383
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-05-12 17:36:30 +0200

    removed datapath global var

--- pseudo_app/app.py
+++ pseudo_app/app.py
@@ -14,9 +14,6 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_core_components as dcc
 
-DATAPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
-
-
 def layout():
     """
     Returns page layout. Modifications of page structure should be passed here
