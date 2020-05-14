ea56569adf273bb979794b746339ceab527978d0
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-05-14 13:00:51 +0200

    corrections Julien
    From https://github.com/psorianom/pseudo_app/issues/3

--- pseudo_app/components/tab_about.py
+++ pseudo_app/components/tab_about.py
@@ -12,23 +12,20 @@ de données par des données indirectement identifiantes**.
 POURQUOI = dcc.Markdown("""
 La loi [n°2016-1321](https://www.legifrance.gouv.fr/affichLoiPubliee.do?idDocument=JORFDOLE000031589829&type=general&legislature=14)
 pour une République numérique fait de l’ouverture des données publiques la règle par défaut. Cela dit,
-l'occultation préalable des éléments à caractère personnel est [une obligation légale](https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000033205514&cidTexte=LEGITEXT000031366350&dateTexte=20161009)
+l'occultation préalable des éléments à caractère personnel est généralement [une obligation légale](https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000033205514&cidTexte=LEGITEXT000031366350&dateTexte=20161009)
 qui s’impose aux administrations.
-Afin de faciliter l'ouverture au grand public, une solution automatisée de pseudonymisation, comme celle-ci, est donc
-souhaitable pour faire face à la grande quantité des documents à traiter dans un temps raisonnable. 
+Afin de faciliter l'ouverture au grand public des documents administratifs sous forme de texte libre et contenant des données personnelles, une solution automatisée de pseudonymisation, comme celle-ci, est donc
+souhaitable pour faire face à la grande quantité des documents à traiter dans un temps raisonnable. Cette solution a été spécifiquement conçue pour les décisions de justice du Conseil d'État.
 
-Cette démo, [l'API](https://github.com/psorianom/pseudo_api), et la [guide pseudonymisation](https://guides.etalab.gouv.fr/pseudonymisation/)
-(où ous pouvez trouver beaucoup plus d'informations) font partie des outils 
- mutuaisables du [Lab IA](https://www.etalab.gouv.fr/datasciences-et-intelligence-artificielle)
-[d'Etalab](https://www.etalab.gouv.fr).
-
+Cette démo, [l'API](https://github.com/psorianom/pseudo_api) sur laquelle elle s'appuie, et le [guide pseudonymisation](https://guides.etalab.gouv.fr/pseudonymisation/) dans lequel vous trouverez plus d'informations sur la démarche font partie des outils mutualisés développés par le [Lab IA](https://www.etalab.gouv.fr/datasciences-et-intelligence-artificielle) [d'Etalab](https://www.etalab.gouv.fr).
 """)
 
 COMMENT = dcc.Markdown("""
-La pseudonymisation peut être vu comme un problème de traitement de langage naturel, plus particulièrement,
+La pseudonymisation peut être vue comme un problème de traitement de langage naturel, plus particulièrement,
 comme une tache de [reconnaissance d'entités nommées](https://fr.wikipedia.org/wiki/Reconnaissance_d%27entit%C3%A9s_nomm%C3%A9es)
-(REN). La REN est souvent résolue à l'aide des méthodes d'apprentissage supervisé séquentiel. Cette démo utilise une approche de ce type,
-c'est-à-dire, nous avons entraîné un modèle en utilisant des décisions de justice annotées indirectement. Le modèle est
+(REN). La REN est souvent résolue à l'aide des méthodes d'apprentissage supervisé séquentiel. Cette démo utilise une approche de ce type : 
+nous avons entraîné un modèle de REN en utilisant des décisions de justice annotées indirectement (les annotations dans ces documents ne provienent pas d'une campagne d'annotation).
+Le modèle est
 ensuite normalement capable de repérer des entités (noms, prènoms, adresses) dans des nouveaux documents. 
 
  
@@ -38,7 +35,7 @@ ensuite normalement capable de repérer des entités (noms, prènoms, adresses) 
 UTILISER = dcc.Markdown("""
 * Vous pouvez uploader un document et ensuite regarder les entités repérées dans le texte ainsi que le texte pseudonymisé
 sur l'onglet **Pseudonymisez un document**.
-* La quantité des documents annotés à la main a une certaine relation avec la qualité de l'annotation automatique. Trouvez un
+* La qualité de la reconnaissance automatique d'entités est directement liée à la qualité (et quantité) de documents annotés à la main. Trouvez un
 exemple de cette relation dans l'onglet **Volume des données annotées**.
 * Dans **Stats** nous affichons les statistiques d'utilisation de notre API de pseudonymisation.
 """)
@@ -51,13 +48,13 @@ tab_about_content = dbc.Tab(
     label='À propos',
     tab_id="tab-about",
         children=html.Div(className='page', children=[
-        html.H4(className='what-is', children='La pseudonymisation en quelque mots'),
+        html.H4(className='what-is', children="Qu'est-ce-que la pseudonymisation ?"),
         html.P(QUOI),
         html.H4(className='what-is', children='Pourquoi un outil de pseudonymisation ?'),
         html.P(POURQUOI),
         html.H4(className='what-is', children='Comment ça marche ?'),
         html.P(COMMENT),
-        html.H4(className='what-is', children='Utiliser cette démo'),
+        html.H4(className='what-is', children='Que contient cette démo ?'),
         html.P(UTILISER),
     ])
 )
